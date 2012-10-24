b61124d74c08ee9834ed10a1ea1d08c7f6f09afb
Evellex <evellex@github.com>
2012-10-24 19:27:45 +1100

    Adding ChasingEnemyAI.cs code

---
+++ ChasingEnemyAI.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+using System.Collections;
+
+public class ChasingEnemyAI : MonoBehaviour {
+	
+	float timer;
+	float walkTimeLength = 1;
+	float walkSpeed = 10;
+	public float patrolRadius = 30;
+	public Vector3 randDest = new Vector3();
+	public Vector3 storeFirstPosition = new Vector3();
+	
+	public GameObject player;
+	// Use this for initialization
+	void Start () {
+	
+		storeFirstPosition = transform.position;
+		
+	randDest = (Random.insideUnitSphere * patrolRadius )+storeFirstPosition;
+	randDest.y = storeFirstPosition.y;
+		
+		transform.LookAt(randDest);
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		if (transform.position != randDest)
+		{
+			transform.position = Vector3.MoveTowards(transform.position,randDest,walkSpeed*Time.deltaTime);
+		}
+		else
+		{
+			randDest = (Random.insideUnitSphere * 30)+storeFirstPosition;
+			randDest.y = storeFirstPosition.y;
+			transform.LookAt(randDest);
+		}
+
+	}
+	/*void OnTriggerExit(Collider collider)
+	{
+		transform.Rotate(0,180,0);
+	}*/
+}
