96e5c16abe37b5abac2520354cf4882f9dcada42
Evellex <evellex@github.com>
2012-10-25 20:39:27 +1100

    Adding sprockets

--- CollectingManager.cs
+++ CollectingManager.cs
@@ -6,6 +6,9 @@ public class CollectingManager : MonoBehaviour {
 	public static int mushroomsCollected;
 	public static int mushroomsToCollect;
 	
+	public static int sprocketsToCollect, sprocketsCollected;
+	
+	
 	// Use this for initialization
 	void Start () {
 	
@@ -25,6 +28,11 @@ public class CollectingManager : MonoBehaviour {
 			Destroy(collider.gameObject);
 			Debug.Log("Deleting mushroom");
 			audio.Play();
+		}
+		if (collider.tag =="Sprocket")
+		{
+			Destroy(collider.gameObject);
+			sprocketsCollected++;
 		}
 	}
 }
