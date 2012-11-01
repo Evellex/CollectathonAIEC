fe6a342779d5abdf992fb28f01368f8f96bed899
Robertodede <Robertodede@github.com>
2012-11-01 17:58:54 +1100

    adding spikes to kill the player

---
+++ spikesKilling.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using System.Collections;
+
+public class spikesKilling : MonoBehaviour
+{
+
+	// Use this for initialization
+	void Start ()
+	{
+		playerdeath.timeToRespawn = 0.3f;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		
+	}
+	void OnCollisionEnter(Collision collision)
+	{
+		if ( collision.gameObject.tag == "Player")
+		{
+			playerdeath.playerAlive = false;
+		}
+	}
+}
+
